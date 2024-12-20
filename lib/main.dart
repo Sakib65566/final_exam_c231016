67b70c433689a42967d2692a5eb4aaeb115cad51
Sakib65566 <Chysakib703@gmail.com>
2024-12-20 21:57:19 +0600

    Md Sakib Chy/C231016

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:final_exam_c231016/view/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'controller/student_controller.dart';
+
+void main() {
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
+  Get.put(StudentsController());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
+  }
+}
